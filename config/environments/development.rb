e25ee5baa1ef978baeb76262e4a646864f7e7b99
Cuadra1107 <r.cuadra2010@gmail.com>
2016-06-23 10:44:04 -0600

    Add files via upload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = :smtp
   config.action_mailer.smtp_settings = { :address => "smtp.gmail.com",
      :port => "587", :domain => "gmail.com", :user_name => "r.cuadra2010@gmail.com", 
-    :password => "Betsa1126", :authentication => "plain", :enable_starttls_auto => true }
+    :password => "password", :authentication => "plain", :enable_starttls_auto => true }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
